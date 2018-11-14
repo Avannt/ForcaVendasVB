2b1c9acf2b74a495a13f6d1c850246280cd50254
P2000753154 <diego.djeri@live.com>
2018-11-14 10:35:43 +0000

    Alterações na view aprovacoes

--- webapp/controller/Aprovacoes.controller.js
+++ webapp/controller/Aprovacoes.controller.js
@@ -5,13 +5,21 @@ sap.ui.define([
 
 	return Controller.extend("testeui5.controller.Aprovacoes", {
 
-			onInit: function() {
-		
-			},
+		onInit: function () {
 
-			onExit: function() {
+		},
 		
-			}
+		onNavBack: function () {
+
+			sap.ui.core.UIComponent.getRouterFor(this).navTo("menu");
+			// this.getOwnerComponent().getModel("modelAux").setProperty("/NrPedCli", "");
+			// this.onResetaCamposPrePedido();
+
+		},
+
+		onExit: function () {
+
+		}
 
 	});
 
