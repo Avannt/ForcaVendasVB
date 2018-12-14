3e9345c224fb7eff1ccee4c8c36a972c18a02710
P2000753154 <diego.djeri@live.com>
2018-12-14 21:39:52 +0000

    Variaveis para ocultar campos - Pedido de Vendas

--- webapp/Component.js
+++ webapp/Component.js
@@ -21,6 +21,8 @@ sap.ui.define([
 			var oModel = new sap.ui.model.json.JSONModel("./model/Menu.json");
     		this.setModel(oModel,"menu");
     		
+    		sap.ui.getCore().getConfiguration().setLanguage("pt-BR");
+    		
     	   // set the device model
 			this.setModel(models.createDeviceModel(), "device");
 
